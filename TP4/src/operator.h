4c6ffb1df2ed60dc1439c0888a0774f5631e63cf
BenjaminFRA4 <skeyben@gmail.com>
2025-12-01 14:15:20 +0100

    Update operator.h

---
+++ TP4/src/operator.h
@@ -0,0 +1,14 @@
+#ifndef OPERATOR_H
+#define OPERATOR_H
+
+// Déclarations des prototypes de fonctions de calcul
+int somme(int num1, int num2);
+int difference(int num1, int num2);
+int produit(int num1, int num2);
+int quotient(int num1, int num2);
+int modulo(int num1, int num2);
+int op_et(int num1, int num2);
+int op_ou(int num1, int num2);
+int op_negation(int num1, int num2); 
+
+#endif // OPERATOR_H
